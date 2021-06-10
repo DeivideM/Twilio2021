6e05e8aa4371be0646567f0f1d5b5e66a47e7366
Deivide Martins <deivide_martins@hotmail.com>
2021-06-10 04:35:20 -0400

    Initial check-in of module Twilio2021

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account; //dotnet add package Twilio
+
+namespace Twilio2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string accountSid = "SeuAccountID";//Configurando separadamente do código Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            string authToken = "SeuAuthToken";//Configurando separadamente do código Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+            TwilioClient.Init(accountSid, authToken);
+
+            //body: mensagem que deseja enviar
+            //Twilio.Types.PhoneNumber: número que você precisa configurar no twilio
+            //to: pra quem você deseja enviar a mensagem
+            var message = MessageResource.Create(
+                body: "Testando envio de mensagem com o twilio.",
+                from: new Twilio.Types.PhoneNumber("+SeuNumeroTwilio"),
+                statusCallback: new Uri("http://postb.in/1234abcd"),
+                to: new Twilio.Types.PhoneNumber("+NumeroQueDesejaEnviarMensagem")
+            );
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
